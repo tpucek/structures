613f9ae1f819ab9c33b0996075aa03bd64a463a6
Szymon Rozkocha <szymon@rozkocha.pl>
2017-10-30 18:54:43 +0100

    ArrayStack

---
+++ src/main/java/ArrayStack.java
@@ -0,0 +1,60 @@
+import java.util.EmptyStackException;
+import java.util.function.Function;
+
+public class ArrayStack<Type> implements Stack<Type> {
+    private Function<Integer, Type[]> supplier;
+    private Type[] array;
+    private int length;
+    private int sizeToAdd;
+
+    public ArrayStack(int size, int sizeToAdd, Function<Integer, Type[]> supplier) {
+        this.supplier = supplier;
+        array = supplier.apply(size);
+        length = 0;
+        this.sizeToAdd = sizeToAdd;
+    }
+
+    @Override
+    public void push(Type a) {
+        if(length == array.length) {
+            addSize(sizeToAdd);
+        }
+        array[length] = a;
+        ++length;
+    }
+
+    public void addSize(int add) {
+        Type[] temp = supplier.apply(length + add);
+
+        System.arraycopy(array, 0, temp, 0, array.length);
+
+        array = temp;
+    }
+
+    @Override
+    public Type pop() {
+        if(length <= 0) {
+            throw new EmptyStackException();
+        }
+        Type temp = array[length - 1];
+        --length;
+        return temp;
+    }
+
+    @Override
+    public String toString() {
+        if(length == 0) {
+            return "[]";
+        }
+
+        StringBuilder textBuilder = new StringBuilder("[");
+        for(int i = 0; i < length - 1; ++i) {
+            textBuilder.append(array[i]).append(", ");
+        }
+
+        textBuilder.append(array[length - 1]).append("]");
+
+        return textBuilder.toString();
+    }
+}
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,42 +4,65 @@ public class Main {
     public static void main(String[] args) {
         Random random = new Random();
 
-        /*List<Integer> test = new ArrayList<Integer>(10, 5, (Integer size) -> {
+        /*List<Integer> arrayList = new ArrayList<Integer>(10, 5, (Integer size) -> {
             return new Integer[size];
         });*/
 
-        List<Integer> test = new ArrayList<Integer>(10, 5, size -> new Integer[size]);
+        System.out.println("ArrayList:");
+
+        List<Integer> arrayList = new ArrayList<Integer>(10, 5, size -> new Integer[size]);
 
         for(int i = 0;i < 10;++i) {
-            test.add(random.nextInt(100));
+            arrayList.add(random.nextInt(100));
         }
 
-        System.out.println(test);
-        test.remove(9);
-        System.out.println(test);
-        test.remove(1);
-        System.out.println(test);
-        test.remove(0);
-        System.out.println(test);
+        System.out.println(arrayList);
+        arrayList.remove(9);
+        System.out.println(arrayList);
+        arrayList.remove(1);
+        System.out.println(arrayList);
+        arrayList.remove(0);
+        System.out.println(arrayList);
 
-        List<Double> test2 = new LinkedList<>();
-        test2.add(random.nextInt(1000) / 10.0);
-        System.out.println(test2);
-        test2.remove(0);
-        System.out.println(test2);
+        System.out.println("");
+        System.out.println("LinkedList:");
 
-        /*Stack stack = new LinkedStack();
-        stack.push(random.nextInt(1000) / 10.0);
-        System.out.println(stack);
-        stack.push(random.nextInt(1000) / 10.0);
-        stack.push(random.nextInt(1000) / 10.0);
-        System.out.println(stack);
-        System.out.println(stack.pop());
-        System.out.println(stack);
-        stack.pop();
-        stack.pop();
-        //stack.pop();
-        System.out.println(stack);
-        Queue<Integer> a;*/
+        List<Double> linkedList = new LinkedList<>();
+        linkedList.add(random.nextInt(1000) / 10.0);
+        System.out.println(linkedList);
+        linkedList.remove(0);
+        System.out.println(linkedList);
+
+        System.out.println("");
+        System.out.println("LinkedStack:");
+
+        Stack<Double> linkedStack = new LinkedStack<>();
+        linkedStack.push(random.nextInt(1000) / 10.0);
+        System.out.println(linkedStack);
+        linkedStack.push(random.nextInt(1000) / 10.0);
+        linkedStack.push(random.nextInt(1000) / 10.0);
+        System.out.println(linkedStack);
+        System.out.println(linkedStack.pop());
+        System.out.println(linkedStack);
+        linkedStack.pop();
+        linkedStack.pop();
+        //linkedStack.pop();
+        System.out.println(linkedStack);
+
+        System.out.println("");
+        System.out.println("LinkedStack:");
+
+        Stack<Integer> arrayStack = new ArrayStack<>(10, 5, size -> new Integer[size]);
+        arrayStack.push(random.nextInt(100));
+        System.out.println(arrayStack);
+        arrayStack.push(random.nextInt(100));
+        arrayStack.push(random.nextInt(100));
+        System.out.println(arrayStack);
+        System.out.println(arrayStack.pop());
+        System.out.println(arrayStack);
+        arrayStack.pop();
+        arrayStack.pop();
+        //arrayStack.pop();
+        System.out.println(arrayStack);
     }
 }
